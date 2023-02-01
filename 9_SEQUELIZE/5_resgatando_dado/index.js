a61942d7d1ac5d2b450b1e04c5529d2ae13753cc
DanielAntunes <antunesdaniel083@gmail.com>
2023-02-01 17:32:14 -0300

    Atualização

--- 9_SEQUELIZE/5_resgatando_dado/index.js
+++ 9_SEQUELIZE/5_resgatando_dado/index.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const conn = require('./db/conn')
 
 const User = require('./models/User')
+const Address = require('./models/Address')
 
 const app = express()
 
@@ -56,6 +57,42 @@ app.post('/users/delete/:id', async (req,res) => {
     res.redirect('/')
 })
 
+// editando dados
+app.get('/users/edit/:id', async (req,res) => {
+    const id = req.params.id
+
+    try {
+        const user = await User.findOne({include: Address, where: {id:id}})
+
+        res.render('useredit', { user: user.get({plain: true}) })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.post('/users/update', async (req,res) => {
+    const id = req.body.id
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if (newsletter === 'on') {
+        newsletter = true
+    } else {
+        newsletter = false
+    }
+
+    const userData = {
+        id,
+        name,
+        occupation,
+        newsletter
+    }
+
+    await User.update(userData, { where: {id:id} })
+    res.redirect('/')
+})
+
 app.get('/', async (req,res) => {
     const users = await User.findAll({raw: true})
 
@@ -64,6 +101,39 @@ app.get('/', async (req,res) => {
     res.render('home', {users: users})
 })
 
-conn.sync().then(() => {
+app.post('/address/create', async (req,res) => {
+    const UserId = req.body.UserId
+    const street = req.body.street
+    const number = req.body.number
+    const city = req.body.city
+
+    const address = {
+        UserId,
+        street,
+        number,
+        city
+    }
+
+    // dados relacionados
+    await Address.create(address)
+
+    res.redirect(`/users/edit/${UserId}`)
+})
+
+app.post('/address/delete', async (req,res) => {
+    const UserId = req.body.UserId
+    const id = req.body.id
+
+    await Address.destroy({
+        where: { id:id }
+    })
+
+    res.redirect(`/users/edit/${UserId}`)
+})
+
+conn
+.sync()
+//.sync({force: true})
+.then(() => {
     app.listen(3000, () => console.log('app rodando na porta 3000'))
 }).catch((err) => console.log(err))
